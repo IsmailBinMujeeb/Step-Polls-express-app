c008353165e41b25afc31b1a535b76d50257d871
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2024-12-31 18:53:04 +0530

    Add basic frontend

---
+++ public/javascripts/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarToggle = document.getElementById('navbar-toggle');
+    const navbarMenu = document.getElementById('navbar-menu');
+  
+    navbarToggle.addEventListener('click', () => {
+      navbarMenu.classList.toggle('show');
+    });
+  });
+  
